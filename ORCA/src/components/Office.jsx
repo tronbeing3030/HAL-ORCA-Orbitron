4e066a5219d1f5831d28b35edd18f4f05dfdb4e3
tronbeing3030 <kaiwarharsha@gmail.com>
2025-02-01 19:16:22 +0530

    Office Prototype

--- ORCA/src/components/Office.jsx
+++ ORCA/src/components/Office.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Office.css'
 import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
 export const Office = () => {
     const location = useLocation()
@@ -9,6 +10,20 @@ export const Office = () => {
         const inputValue = (evt) => {
             setValue(evt.target.value);
         }
+
+    const loop = () => {
+        const employees = [];
+        for(let i = 0; i < 200; i++){
+        employee = {
+          "_id": 1000 + i,
+          "employee_name": "Employee" + i,
+          "designation": random.choice(designations),
+          "seat_id": str(200 + i) 
+          }
+          employees.push(employee)
+        }
+        return Array.map((employee) => <div style={{border: "1px solid white"}}></div>)
+      }
   return (
     <>
       <div className="top">
@@ -16,16 +31,17 @@ export const Office = () => {
         <div><button>Save</button></div>
       </div>
       <div className="bottom">
-        <div className="left"></div>
+        <div className="left">{[1,2,3]}</div>
         <div className="right">
           <div className="seats">
             <h2>TOTAL SEATS</h2>
             <p>150</p></div>
           <div className="info">
             <div className="form">
-            <label>SEAT ID:</label> <input value='###########' />
-            <input type="text" placeholder='Employee Name' />
-            <input type="text" placeholder='Designation'/>
+            <label>SEAT ID:</label> <input placeholder='##########' onChange={inputValue} />
+            <input type="text" placeholder='Employee Name' onChange={inputValue} />
+            <input type="text" placeholder='Designation' onChange={inputValue}/>
+            <button>SUBMIT</button>
             </div>
           </div>
         </div>
